2010e9d68396fa9092b02955dbbbecc67a2470c9
BradHuang <adghbc01@tamu.edu>
2017-02-26 06:35:35 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,10 @@
 class MoviesController < ApplicationController
-
+  def initialize
+     @all_ratings = Movie.all_ratings
+     @rating_selected = @all_ratings.map { |rating| [rating, true] }.to_h
+     super
+  end
+   
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
@@ -11,12 +16,18 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @sort = params[:sort_by]
-    if @sort == 'title' || @sort == 'release_date'
-       @movies = Movie.all.order(@sort)
-       eval("@sort_by_" + @sort + " = 'hilite'")
-    else
-       @movies = Movie.all
+      @movies = Movie.all
+      ratings = params[:ratings]
+   if ratings
+      ratings = ratings.keys
+      @movies = @movies.where(rating: ratings)
+      @all_ratings.each do |rating|
+      @rating_selected[rating] = ratings.include? rating
+ +    end
+   end
+  sort = params[:sort_by]
+    if sort == 'title' || sort == 'release_date'
+       @movies = @movies.order(sort)
     end
   end
 
